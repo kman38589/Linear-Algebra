066cba4bd29898835a52d0761211770c138fdae6
kman38589 <gunit38589@aol.com>
2015-10-29 18:43:16 -0400

    Update qrcgs.jl
    adde what worked in my ijulia notebook

--- qrcgs.jl
+++ qrcgs.jl
@@ -12,3 +12,18 @@ function qrcgs(A)
         Q[:,j]=v/R[j,j];
     end
 end
+function qrcgs2(A)
+    (m,~)=size(A);
+    R=zeros(m,m);
+    Q=zeros(m,m);
+    for j=1:m
+        v=A[:,j];
+        for i= 1:j-1 
+            R[i,j]=(Q[:,i]'*A[:,j])[1];
+            v=v-R[i,j]*Q[:,i];
+        end
+        R[j,j]=norm(v);
+        Q[: ,j]=v/R[j,j];
+    end
+    return (Q,R)
+end

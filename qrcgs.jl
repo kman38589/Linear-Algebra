7fd761c78898819de4955a3b17ad6dc3df84f3a7
acarta <adc1009@wildcats.unh.edu>
2015-10-29 18:23:23 -0400

    Create qrcgs.jl
    Given a matrix A, execute classical Gram-Schmidt to produce QR decomposition of A.

---
+++ qrcgs.jl
@@ -0,0 +1,14 @@
+function qrcgs(A)
+    (m,n)=size(A);
+    R=zeros(m,m);
+    Q=zeros(m,m);
+    for j=1:n
+        v=A[:,j];
+        for i=1:j-1
+            R[i,j]=(Q[:,i]'*A[:,j])[1];
+            v=v-R[i,j]*Q[:,i];
+        end
+        R[j,j]=norm(v);
+        Q[:,j]=v/R[j,j];
+    end
+end
